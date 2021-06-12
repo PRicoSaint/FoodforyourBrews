5a82ad462568443ed71964e5cff447daf8a74fd0
PRicoSaint <santosesteban09@gmail.com>
2021-06-12 11:32:32 -0500

    added initial api pull request for random beers

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
-var food = 22;
+
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.punkapi.com/v2/beers/random';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        // console
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+  })
+};
+
+getApi();
+
